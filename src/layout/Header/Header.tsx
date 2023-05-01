69d7c5b58aab8f6d9ef20c55dd7bb9ffa9218100
Kirill Starodubsev <colereteps@gmail.com>
2023-05-01 05:50:08 +0400

    feat: Update Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,11 +1,20 @@
 import React, { FC } from "react";
 import * as styles from "./Header.module.scss";
 import { Button } from "@components/Button/Button";
+import { Link, navigate } from "gatsby";
 export const Header: FC = (): JSX.Element => {
   return (
     <div className={styles["container"]}>
-      <div className={styles["title"]}>Rick and Morty Shop</div>
-      <Button apperance="unprimary" size="middle">
+      <Link to="/" className={styles["title"]}>
+        Rick and Morty Shop
+      </Link>
+      <Button
+        onClick={() => {
+          navigate("/cart");
+        }}
+        apperance="unprimary"
+        size="middle"
+      >
         Cart
       </Button>
     </div>
